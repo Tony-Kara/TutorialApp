b719e64f14f3094d5aa75901f5660a4fe56bbef6
Tony-Kara <eniolaanthony@gmail.com>
2021-10-06 14:07:10 +0600

    Initial Commit

---
+++ TutorialApp/TutorialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialAppApp.swift
+//  TutorialApp
+//
+//  Created by mac on 10/6/21.
+//
+
+import SwiftUI
+
+@main
+struct TutorialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
