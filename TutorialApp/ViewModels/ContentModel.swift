1fa6844260b9a68f417b934dbedd6fc1a23cf931
Tony-Kara <eniolaanthony@gmail.com>
2021-10-06 19:35:04 +0600

    Add assets, data files add modules, paste json in content model

--- TutorialApp/ViewModels/ContentModel.swift
+++ TutorialApp/ViewModels/ContentModel.swift
@@ -9,4 +9,49 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    var styleData: Data?
+    
+    init(){
+        
+        getlocalData()
+    }
+    
+    func getlocalData() {
+        
+       
+        
+        // Get a url path to the json file
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json") // You can put a breakpoint here to test if you are parsing the data correctly, remerber, if you run into errors, you probably did not map your model properly
+        
+        do {
+        // Read the file into a data object
+            let jsonData = try Data(contentsOf: jsonUrl!)
+       // Try to decode the json into an array of modules
+        let jsonDecoder = JSONDecoder()
+        let modules =  try  jsonDecoder.decode([Module].self, from: jsonData)
+        
+            self.modules =  modules // once you can po modules in the console after you run the simulator, you successfully parsed the data.
+            
+        }
+        catch{
+            print("could'nt parse local data")
+        }
+        
+         
+        //parse the style data
+        
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+       
+        do{
+            //Read the file into a data object, leave it as data object and do not decode it as it is not actually a JSON object
+            let styleData = try Data(contentsOf: styleUrl!)
+            self.styleData = styleData
+        }
+        catch{
+            print("could'nt parse style data")
+        }
+            
+    }
+    
 }
