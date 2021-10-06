fe7f9de78a4050c6856d6e3a6b209a72f691b415
Tony-Kara <eniolaanthony@gmail.com>
2021-10-06 14:29:24 +0600

    - Organize files into folders - add content model - set content model as environment object

--- TutorialApp/TutorialApp.swift
+++ TutorialApp/TutorialApp.swift
@@ -8,10 +8,11 @@
 import SwiftUI
 
 @main
-struct TutorialAppApp: App {
+struct TutorialApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
+                .environmentObject(ContentModel())
         }
     }
 }
---
+++ TutorialApp/ViewModels/ContentModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentModel.swift
+//  TutorialApp
+//
+//  Created by mac on 10/6/21.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+}
